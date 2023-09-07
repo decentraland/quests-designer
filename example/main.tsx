6a971d4d9231b8f2e6d9412f078406fea76136b0
lauti7 <lautigbustos@gmail.com>
2023-09-06 23:33:55 -0300

    feat: example as figma

--- example/main.tsx
+++ example/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         console.log(nodes)
         console.log(edges)
         alert("Saved")
-      } }} 
+      }, content: "Generate Quest" }} 
     />
   </React.StrictMode>
 )
